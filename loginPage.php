ccc52136b8f378b5ea250b8519a7b58107424441
JAKE <christan.jake2025@yahoo.com>
2014-08-05 13:11:59 +0800

    modify login title

--- loginPage.php
+++ loginPage.php
@@ -42,7 +42,7 @@
       <nav class="top-bar" data-topbar>
         <ul class="title-area">
           <li class="name">
-            <h1><a href="#">Northstar Admin Login</a></h1>
+            <h1><a href="#">Northstar Admin Login Page</a></h1>
           </li>
           <li class="toggle-topbar"><a href="#"><span>Menu</span></a></li>
         </ul>
